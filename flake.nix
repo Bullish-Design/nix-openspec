1d258bed3118247bf526791ee0d0d29fd358657c
Bullish-Design <97364799+Bullish-Design@users.noreply.github.com>
2026-01-06 20:21:41 -0500

    Add devenv flake for openspec

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "OpenSpec LLM agent framework development environment";
+
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    devenv.url = "github:cachix/devenv";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, devenv, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = devenv.lib.mkShell {
+          inherit pkgs;
+          inputs = { inherit nixpkgs; };
+          modules = [
+            ./devenv.nix
+          ];
+        };
+      });
+}
